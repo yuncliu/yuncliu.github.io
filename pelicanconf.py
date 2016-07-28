18071d9973f1eaf85c08d8ad43788d753545e88f
yuncliu <lyc1990@qq.com>
2016-07-28 02:11:38 -0400

    change theme, add toc

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,10 +26,16 @@ LINKS = (('Pelican', 'http://getpelican.com/'),)
 # Social widget
 SOCIAL = ()
 
-PLUGINS = ["pelican_plugin-render_math", "tag_cloud"]
-THEME="./materialistic-pelican"
+PLUGINS = ["pelican_plugin-render_math", "tag_cloud", "pelican-toc"]
+THEME="pelican-bootstrap3"
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+TOC = {
+    'TOC_HEADERS' : '^h[1-6]',  # What headers should be included in the generated toc
+                                # Expected format is a regular expression
+    'TOC_RUN'     : 'true'      # Default value for toc generation, if it does not evaluate
+                                # to 'true' no toc will be generated
+}
